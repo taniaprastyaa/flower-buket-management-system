ce94443099a9b6b1ac9a328d35a4697c44affd06
taniaprastyaa <87980415+taniaprastyaa@users.noreply.github.com>
2025-04-14 14:14:49 +0800

    feat: Delete unimportant file

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,3 @@
-import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
-import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
-import { hasEnvVars } from "@/utils/supabase/check-env-vars";
 import { redirect } from "next/navigation";
 
 export default async function Home() {
